842f2ad4491867c6c4d58cb30c7e957e7d5a2b87
sivothajan <sivothajan2002@gmail.com>
2025-07-12 11:40:15 +0530

    feat: add binanceClient for API key management and update import in getDepositAddress

--- api/binance/getDepositAddress.js
+++ api/binance/getDepositAddress.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 import { createHmac } from "crypto";
 import AbortController from "abort-controller";
-import { apiKey, apiSecret } from "./binanceCilent.js";
+import { apiKey, apiSecret } from "./binanceClient.js";
 
 if (!apiKey || !apiSecret) {
   console.error("API Key or Secret is missing!");
