cb20f2e9548084672ff8e03316764e38c4504d8c
sivothajan <sivothajan2002@gmail.com>
2025-07-12 11:50:39 +0530

    fix: export supabaseTable for better accessibility and update export structure

--- api/supabase/supabaseClient.js
+++ api/supabase/supabaseClient.js
@@ -5,8 +5,6 @@ dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabasePublicAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabaseTable = process.env.SUPABASE_TABLE;
+export const supabaseTable = process.env.SUPABASE_TABLE;
 
-const supabase = createClient(supabaseUrl, supabasePublicAnonKey);
-
-export default { supabase, supabaseTable };
+export const supabase = createClient(supabaseUrl, supabasePublicAnonKey);
