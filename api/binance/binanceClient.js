939e22b35c3962646102acd0ec59b15cf290cd57
sivothajan <sivothajan2002@gmail.com>
2025-07-12 11:48:50 +0530

    fix: export apiKey and apiSecret for better accessibility and improve error message

--- api/binance/binanceClient.js
+++ api/binance/binanceClient.js
@@ -2,12 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const apiKey = process.env.BINANCE_API_KEY;
-const apiSecret = process.env.BINANCE_API_SECRET;
+export const apiKey = process.env.BINANCE_API_KEY;
+export const apiSecret = process.env.BINANCE_API_SECRET;
 
 if (!apiKey || !apiSecret) {
-  console.error("Binance API Key or Binance Secret is missing!");
+  console.error("Binance API Key or Secret is missing!");
   process.exit(1);
 }
-
-export default { apiKey, apiSecret };
