3511f92881a5a7473cc69528f79cb9056080efa8
sivothajan <sivothajan2002@gmail.com>
2025-08-12 23:40:17 +0530

    fix: update generateUUID function to ensure consistent UUID generation

--- api/utils/generateUUID.js
+++ api/utils/generateUUID.js
@@ -4,4 +4,4 @@ import { v4 } from "uuid";
  * Generates a UUID (Universally Unique Identifier).
  * This function uses the `uuid` library to create a version 4 UUID.
  */
-export const generateUUID = v4;
+export const generateUUID = () => v4();
